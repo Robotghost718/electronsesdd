75706891bb33a11208d4d5b2f2605d8c0b0981ce
Robotghost <dankmstern@gmail.com>
2020-12-04 15:06:53 -0500

    first commite

--- renderer.js
+++ renderer.js
@@ -21,3 +21,21 @@ serialport.list((err, ports) => {
   tableHTML = tableify(ports)
   document.getElementById('ports').innerHTML = tableHTML
 })
+
+var sp = new serialport('/dev/tty.usbserial-AK08TZHG', {
+  baudRate: 115200,
+});
+function writeonSer(data){
+  //Write the data to serial port.
+  sp.write( data, function(err) {
+      if (err) {
+          return console.log('Error on write: ', err.message);
+      }
+      console.log('message written');
+  });
+
+}
+document.getElementById('button').onclick = function(e){
+  //send ctrl+c to serialport
+  writeonSer('\x03');
+}
